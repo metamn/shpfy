d873a1b74d02dc2546fad6cdf1721e8b088070a2
rubycs <rubycs@clair.ro>
2010-11-03 12:38:40 +0200

    Frontpage ready to be styled

--- basic/assets/jquery.init.js
+++ basic/assets/jquery.init.js
@@ -1,3 +1,11 @@
-$(document).ready(function() { 
-
-};
+jQuery.noConflict();
+     
+// Use jQuery via jQuery(...)
+jQuery(document).ready(function(){
+  
+  // Show excerpt on Frontpage
+  jQuery("#index #info span.more").click(function(){
+    jQuery("#index #info #excerpt").toggle();
+  });
+  
+});   
