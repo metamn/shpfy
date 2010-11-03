d41f6fd568b93c69aac42ec76cdda515858fe1d3
rubycs <rubycs@clair.ro>
2010-11-03 14:27:21 +0200

    Color scheme added

--- basic/assets/jquery.init.js
+++ basic/assets/jquery.init.js
@@ -5,6 +5,13 @@ jQuery(document).ready(function(){
   
   // Show excerpt on Frontpage
   jQuery("#index #info span.more").click(function(){
+    var arrow = jQuery(this).html();     
+    if (arrow == "→") {
+      arrow = "&larr;";
+    } else {
+      arrow = "&rarr;";
+    };
+    jQuery(this).html(arrow);  
     jQuery("#index #info #excerpt").toggle();
   });
   
