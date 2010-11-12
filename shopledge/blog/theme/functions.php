5f3cdf565ee405baf9a6c665c521045925fe5061
rubycs <rubycs@clair.ro>
2010-11-12 18:39:46 +0200

    Shopledge single page designed

--- shopledge/blog/theme/functions.php
+++ shopledge/blog/theme/functions.php
@@ -39,4 +39,46 @@ function post_attachements($post_id) {
   return $attachments;
 }
 
+
+// Styling comments
+// - documentation @ http://codex.wordpress.org/Template_Tags/wp_list_comments
+function styled_comments($comment, $args, $depth){
+  $GLOBALS['comment'] = $comment; ?>
+  
+  <li <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>">
+    <div id="comment-<?php comment_ID(); ?>" class="block">
+      <div class="comment-author vcard column span-4 last">
+         <?php 
+          if ($avatar_results = get_avatar($comment, $size='96')) {
+            if ($avatar_results == 'default_avatar') {
+              printf(__('<cite class="fn">%s</cite>'), get_comment_author_link());
+            }
+            else {
+              echo $avatar_results;
+              echo '<br/>';
+            }
+          } else {
+            printf(__('<cite class="fn">%s</cite>'), get_comment_author_link());
+          } ?>
+      </div>
+      <div class="column last">
+        <?php if ($comment->comment_approved == '0') : ?>
+          <em><?php _e('Your comment is awaiting moderation.') ?></em>
+          <br />
+        <?php endif; ?>
+        
+        <div class="comment-body">
+          <?php comment_text() ?>
+        </div>
+        
+        <div class="comment-meta commentmetadata">
+          <a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php printf(__('%1$s la %2$s'), get_comment_date(),  get_comment_time()) ?></a>
+          <?php edit_comment_link(__('(Modificare)'),'  ','') ?>
+          <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+        </div>        
+      </div>
+    </div>
+<?php  
+}
+
 ?>
