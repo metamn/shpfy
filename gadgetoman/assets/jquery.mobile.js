641d251bf88393964bc8bf62fe7a0014c96ab210
rubycs <rubycs@clair.ro>
2011-02-28 13:57:43 +0200

    Mobile bugs solved

--- gadgetoman/assets/jquery.mobile.js
+++ gadgetoman/assets/jquery.mobile.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function(){
   // Use these functions only when mobile.css is loaded !!
   var mobile = ( jQuery(".mobile").css("display") === "block" );
   if (mobile) {
-    
+       
     // Scrolling featured items on mobile  
     jQuery("#featured .section ul").children().addClass('hidden');
     jQuery("#featured0").removeClass('hidden');
