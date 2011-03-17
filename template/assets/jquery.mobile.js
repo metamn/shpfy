b30fedbc2d06d66bb3b20d462cd6752b3306edf6
rubycs <rubycs@clair.ro>
2011-03-17 19:05:27 +0200

    Starting to extract the Shopify template

---
+++ template/assets/jquery.mobile.js
@@ -0,0 +1,25 @@
+jQuery.noConflict();
+     
+// Use jQuery via jQuery(...)
+jQuery(document).ready(function(){
+
+  // Use these functions only when mobile.css is loaded !!
+  var mobile = ( jQuery(".mobile").css("display") === "block" );
+  if (mobile) {
+       
+    // Scrolling featured items on mobile  
+    jQuery("#featured .section ul.subs").children().addClass('hidden');
+    jQuery("#featured0").removeClass('hidden');
+    
+    jQuery("#featured .nav a").click(function(event){    
+      var target = jQuery(this).attr('href');
+      if (jQuery(this).attr('class') == "next") {
+        jQuery(target).removeClass('hidden');
+      } else {
+        jQuery(this).parent().parent().parent().addClass('hidden');
+      }   
+    });
+  
+  }
+    
+});   
