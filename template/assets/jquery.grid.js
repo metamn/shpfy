b18584dc7547b452e28af89416337fb06e89d445
rubycs <rubycs@clair.ro>
2011-03-19 17:10:14 +0200

    The grids and images done for Template. Template is ready to use

--- template/assets/jquery.grid.js
+++ template/assets/jquery.grid.js
@@ -3,14 +3,19 @@ jQuery.noConflict();
 // Use jQuery via jQuery(...)
 jQuery(document).ready(function(){
   
-  // horizontal grid
   var horizontal = "";
   for (i=0; i<400; i++) {
     horizontal += '<div class="col">&nbsp;</div>';
   }
   
+  var vertical = ""
+  for (i=0; i<1200; i++) {
+    vertical += '<div class="col">&nbsp;</div>';
+  }
+  
   jQuery("body").append(
-    '<div id="grid">' + horizontal + '</div>'  
+    '<div id="gridh" class="grid">' + horizontal + '</div>' +
+    '<div id="gridv" class="grid">' + vertical + '</div>'
   );
     
 });   
