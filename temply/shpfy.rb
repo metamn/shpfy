fbfb38f3f43e116ea05f586dd239f406e9541582
rubycs <rubycs@clair.ro>
2011-03-20 18:41:24 +0200

    Shopify generator added

---
+++ temply/shpfy.rb
@@ -0,0 +1,46 @@
+
+def parse(line)
+  ret = ''
+  
+  args = line.split ' '
+    
+  source = "#{args[1]}"
+  if File.exists? source
+    
+    File.open(source, 'r:UTF-8') do |f|
+      f.each_line do |line|
+        if line.include? "include "
+          ret += parse line 
+        else
+          ret += line.gsub("@arg", "#{args[2]}")
+        end
+      end
+    end
+      
+  else
+    puts "File does not exists: #{source}" 
+  end
+  
+  ret
+end
+
+
+unless ARGV.empty? 
+  arg = ARGV[0]
+  
+  source = "#{arg}.shpfy"
+  if File.exists? source
+    dest= "#{arg}.liquid"
+    
+    ret = parse "n/a #{source} n/a"
+    
+    File.open(dest, 'w') { |f| f.write(ret) } 
+    
+  else
+    puts "File does not exists: #{source}" 
+  end
+else
+  puts "Generate Shopify templates" 
+  puts " - Usage: shpfy filename (without extension, .shpfy is used automaticaly)"
+  puts " - Output files are put into current directory" 
+end
