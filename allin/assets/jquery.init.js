287df8fa019e85bc9a37183a846fd8de7d756cb7
rubycs <rubycs@clair.ro>
2011-03-28 18:23:05 +0300

    Scrolling on frontpage is working for a single section

--- allin/assets/jquery.init.js
+++ allin/assets/jquery.init.js
@@ -4,33 +4,39 @@ jQuery.noConflict();
 jQuery(document).ready(function(){
   
   
+  // Marking the scroller active 
+  jQuery("#section.all #navigation a").click(function() {
+    jQuery("#section.all #navigation").children().removeClass("inactive");
+    jQuery(this).parent().addClass("inactive");
+  });
+  
   // Scrolling products at frontpage
- jQuery.localScroll.defaults.axis = 'xy';
+  jQuery.localScroll.defaults.axis = 'xy';
   jQuery.localScroll.hash({
-    target: '#latest', 
+    target: '#section.all', 
     queue: true,
     duration: 1500
   });
   jQuery.localScroll({
-    target: '#latest', 
+    target: '#section.all', 
     queue: true,
     duration: 1000,
     hash: true,
     onBefore:function( e, anchor, $target ){
-	
+
     },
     onAfter:function( anchor, settings ){
-	
+
     }
   });
   
   // Show product details on hover, at frontpage
   jQuery("#section #article.thumb #figure").hover(
     function () {
-      jQuery(this).next().fadeIn('slow');
+      jQuery(this).next("#summary").fadeIn('slow');
     }, 
     function () {
-      jQuery(this).next().fadeOut('slow');
+      jQuery(this).next('#summary').fadeOut('slow');
     }
   );  
   
