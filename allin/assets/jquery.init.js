fcfffa52ef6aa3d24a2dbcc5d178cb0ef52901e5
rubycs <rubycs@clair.ro>
2011-03-28 13:43:00 +0300

    Article excerpts, tags added to blog

--- allin/assets/jquery.init.js
+++ allin/assets/jquery.init.js
@@ -3,6 +3,37 @@ jQuery.noConflict();
 // Use jQuery via jQuery(...)
 jQuery(document).ready(function(){
   
+  
+  // Scrolling products at frontpage
+ jQuery.localScroll.defaults.axis = 'xy';
+  jQuery.localScroll.hash({
+    target: '#latest', 
+    queue: true,
+    duration: 1500
+  });
+  jQuery.localScroll({
+    target: '#latest', 
+    queue: true,
+    duration: 1000,
+    hash: true,
+    onBefore:function( e, anchor, $target ){
+	
+    },
+    onAfter:function( anchor, settings ){
+	
+    }
+  });
+  
+  // Show product details on hover, at frontpage
+  jQuery("#section #article.thumb #figure").hover(
+    function () {
+      jQuery(this).next().fadeIn('slow');
+    }, 
+    function () {
+      jQuery(this).next().fadeOut('slow');
+    }
+  );  
+  
   // Click on Search in the Main Menu  
   jQuery("#header #nav #main-menu .search").toggle (
     function () {
@@ -12,8 +43,5 @@ jQuery(document).ready(function(){
       jQuery("#header #search").fadeOut('slow');
     }
   );
-    
-  
-   
     
 });   
